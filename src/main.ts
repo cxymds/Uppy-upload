c1bab35d9b29a46268d4fbe4298caaf850db5f0a
cxymds <cxymds@qq.com>
2024-12-24 16:03:17 +0800

    fix：Object.hasOwn is not a function

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import ElementPlus from 'element-plus';
-import zhCn from 'element-plus/es/locale/lang/zh-cn';
-import router from '~/router';
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import router from '~/router'
 // import "~/styles/element/index.scss";
 
-import '~/styles/index.scss';
-import 'uno.css';
+import '~/styles/index.scss'
+import 'uno.css'
 
 // If you want to use ElMessage, import it.
-import 'element-plus/theme-chalk/src/message.scss';
-import 'element-plus/theme-chalk/src/message-box.scss';
+import 'element-plus/theme-chalk/src/message.scss'
+import 'element-plus/theme-chalk/src/message-box.scss'
 
-const app = createApp(App);
+if (!Object.hasOwn) {
+  Object.hasOwn = function (obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop)
+  }
+}
+
+const app = createApp(App)
 app
   .use(ElementPlus, {
-    locale: zhCn,
+    locale: zhCn
   })
-  .use(router);
-app.mount('#app');
+  .use(router)
+app.mount('#app')
